be3979dfdf5a20ed4e33b0ab0d2bb950fb2f197c
Danieldylan1 <shadowbox826@gmail.com>
2017-08-14 11:50:25 -0400

    added some winston support

--- src/bot.js
+++ src/bot.js
@@ -3,8 +3,26 @@ const Discord = require('discord.js');
 const fs = require('file-system');
 const path = require('path');
 const moment = require('moment');
-const log = require('winston');
+const winston = require('winston');
 const firebase = require('firebase');
+
+//Setup winston
+var colors = {
+  levels: {
+    info: 0,
+    debug: 1,
+    warn: 2,
+    critical: 3
+  },
+  colors: {
+    info: 'blue',
+    debug: 'green',
+    warn: 'yellow',
+    critical: 'red'
+  }
+};
+var logger = new (winston.Logger)({ levels: colors.levels });
+winston.addColors(colors.colors)
 
 // Initialize Firebase
 // TODO: Replace with your project's customized code snippet
@@ -26,4 +44,21 @@ try {
 
 //Variables
 let pf = cfg.pf;
-let 
+let info = "**:notepad-spiral: INFO >> **";
+let warning = "**:warning:  WARNING >> **"
+
+//Create Bot Object
+const bot = new Discord.Client();
+
+//Bot Initialization
+bot.login(cfg.token);
+
+//Functions (None Currently)
+
+//Bot Events
+//Bot Variables
+bot.on('ready', => {
+
+});
+let msgArray = message.content.split(" ");
+let command =
